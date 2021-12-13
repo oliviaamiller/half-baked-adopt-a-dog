2cd88cfbd08a39d40b340ae5b04558287b5246e7
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-13 14:11:35 -0800

    initial set up for render function tdd

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,33 @@
 import './example.test.js';
+import { renderDogCard, renderDogDetail } from '../render-utils.js';   
+
+
+const test = QUnit.test;
+
+test('renderDogCard is a function that will return DOM node', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderDogCard(dog);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('renderDogDetail is a function that will return DOM node', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderDogDetail(dog);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
